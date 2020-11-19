2d50214cb9881961cb7304cae7f02df7cd7045eb
hritu567 <hritumog96@gmail.com>
2020-11-19 20:34:15 +0530

    Added : solved UC3 using switch statement

--- utility.js
+++ utility.js
@@ -1,23 +1,22 @@
-//uc3
-//Added part time employee wage
-var IS_FULL_TIME = 1;
-var IS_PART_TIME = 2;
+//uc4
+//solving UC3 using Switch statement
 var EMP_RATE_PER_HOUR = 20;
 var empHrs = 0;
 var empCheck = Math.floor(Math.random() * 10) % 3;
 
-if (empCheck == IS_FULL_TIME) {
-	console.log("Employee is Present for full time");
-	empHrs = 8;
-    } 
-else if (empCheck == IS_PART_TIME) {
-	console.log("Employee is Present for part time");
-	empHrs = 4;
-    } 
-else {
-	console.log("Employee is Absent");
-	empHrs = 0;
-	}
+switch (empCheck) {
+	case 1:
+		console.log("Employee is Present for full time");
+		empHrs = 8;
+		break;
+	case 2:
+		console.log("Employee is Present for part time");
+		empHrs = 4;
+		break;
+	default:
+		console.log("Employee is Absent");
+		empHrs = 0;
+		break;
+		}
 var dailyWage = EMP_RATE_PER_HOUR * empHrs;
 console.log("Employee Daily wage is : " + dailyWage);
-	
