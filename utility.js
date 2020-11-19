27c535824a1460f34d348be5e376304da5df3245
hritu567 <hritumog96@gmail.com>
2020-11-19 20:45:19 +0530

    Added : Calculating employee wage for month

--- utility.js
+++ utility.js
@@ -1,22 +1,23 @@
-//uc4
-//solving UC3 using Switch statement
+//uc5
+//Calculating Wages for month
 var EMP_RATE_PER_HOUR = 20;
+var NUM_OF_WORKING_DAYS = 20;
 var empHrs = 0;
-var empCheck = Math.floor(Math.random() * 10) % 3;
-
-switch (empCheck) {
-	case 1:
-		console.log("Employee is Present for full time");
-		empHrs = 8;
-		break;
-	case 2:
-		console.log("Employee is Present for part time");
-		empHrs = 4;
-		break;
-	default:
-		console.log("Employee is Absent");
-		empHrs = 0;
-		break;
+var totalSalary = 0;
+for (let day = 1; day <= NUM_OF_WORKING_DAYS; day++) {
+	var empCheck = Math.floor(Math.random() * 10) % 3;
+	switch (empCheck) {
+		case 1:
+			empHrs = 8;
+			break;
+		case 2:
+			empHrs = 4;
+			break;
+		default:
+			empHrs = 0;
+			break;
+			}
+	var dailyWage = EMP_RATE_PER_HOUR * empHrs;
+	totalSalary = totalSalary + dailyWage;
 		}
-var dailyWage = EMP_RATE_PER_HOUR * empHrs;
-console.log("Employee Daily wage is : " + dailyWage);
+console.log("Total Salary for a month is : " + totalSalary);
