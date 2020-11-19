5acc69756f52c85506e054dfb695f8be3d118410
hritu567 <hritumog96@gmail.com>
2020-11-19 19:51:47 +0530

    Added : Calculating Daily Employee Wage

--- utility.js
+++ utility.js
@@ -1,9 +1,14 @@
-//uc1
-//Employee present or not
+//uc2
+//Calculating daily employee Wage
 var IS_FULL_TIME=1;
-     var empCheck=Math.floor(Math.random() * 10) % 2;    
-     if (empCheck == IS_FULL_TIME)
-    	 console.log("Employee is Present");
-     else
-    	 console.log("Employee is Absent");
+var EMP_RATE_PER_HOUR=20;
+var empHrs=0;
+var empWage=0;
+var empCheck=Math.floor(Math.random() * 10) %2;
+if (empCheck == IS_FULL_TIME)
+	empHrs=8;
+else
+	empHrs=0;
+empWage = empHrs * EMP_RATE_PER_HOUR;
+console.log("Emp Wage: " + empWage);
 	
