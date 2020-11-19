72c9604825778a3e48b5162ca9c25c585c87d972
hritu567 <hritumog96@gmail.com>
2020-11-19 18:32:14 +0530

    Added : Employee present or not

---
+++ utility.js
@@ -0,0 +1,9 @@
+//uc1
+//Employee present or not
+var IS_FULL_TIME=1;
+     var empCheck=Math.floor(Math.random() * 10) % 2;    
+     if (empCheck == IS_FULL_TIME)
+    	 console.log("Employee is Present");
+     else
+    	 console.log("Employee is Absent");
+	
