19e7015172d295953c32ac0d40c5cdfefc5646db
hritu567 <hritumog96@gmail.com>
2020-11-19 22:55:12 +0530

    Added : calculated wages till total working hours or days are reached

--- utility.js
+++ utility.js
@@ -1,11 +1,17 @@
-//uc5
-//Calculating Wages for month
-var EMP_RATE_PER_HOUR = 20;
-var NUM_OF_WORKING_DAYS = 20;
+//uc6
+//Calculating Wages till the condition of total working hours or days reached
+const IS_FULL_TIME = 1;
+const IS_PART_TIME = 2;
+const EMP_RATE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 100;
 var empHrs = 0;
 var totalSalary = 0;
-for (let day = 1; day <= NUM_OF_WORKING_DAYS; day++) {
-	var empCheck = Math.floor(Math.random() * 10) % 3;
+var totalEmpHrs = 0;
+var totalWorkingDays = 0;
+while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    var empCheck = Math.floor(Math.random() * 10) % 3;
 	switch (empCheck) {
 		case 1:
 			empHrs = 8;
@@ -16,8 +22,10 @@ for (let day = 1; day <= NUM_OF_WORKING_DAYS; day++) {
 		default:
 			empHrs = 0;
 			break;
-			}
-	var dailyWage = EMP_RATE_PER_HOUR * empHrs;
-	totalSalary = totalSalary + dailyWage;
-		}
+            }
+            totalEmpHrs += empHrs;
+			console.log("Day : " + totalWorkingDays + " Emp hours : " + empHrs);
+    
+}
+totalSalary = EMP_RATE_PER_HOUR * totalEmpHrs;
 console.log("Total Salary for a month is : " + totalSalary);
