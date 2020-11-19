698747a45465c7a2222c9fefe06f95738e3137ee
hritu567 <hritumog96@gmail.com>
2020-11-19 20:26:49 +0530

    Added : Part time employee wage

--- utility.js
+++ utility.js
@@ -1,14 +1,23 @@
-//uc2
-//Calculating daily employee Wage
-var IS_FULL_TIME=1;
-var EMP_RATE_PER_HOUR=20;
-var empHrs=0;
-var empWage=0;
-var empCheck=Math.floor(Math.random() * 10) %2;
-if (empCheck == IS_FULL_TIME)
-	empHrs=8;
-else
-	empHrs=0;
-empWage = empHrs * EMP_RATE_PER_HOUR;
-console.log("Emp Wage: " + empWage);
+//uc3
+//Added part time employee wage
+var IS_FULL_TIME = 1;
+var IS_PART_TIME = 2;
+var EMP_RATE_PER_HOUR = 20;
+var empHrs = 0;
+var empCheck = Math.floor(Math.random() * 10) % 3;
+
+if (empCheck == IS_FULL_TIME) {
+	console.log("Employee is Present for full time");
+	empHrs = 8;
+    } 
+else if (empCheck == IS_PART_TIME) {
+	console.log("Employee is Present for part time");
+	empHrs = 4;
+    } 
+else {
+	console.log("Employee is Absent");
+	empHrs = 0;
+	}
+var dailyWage = EMP_RATE_PER_HOUR * empHrs;
+console.log("Employee Daily wage is : " + dailyWage);
 	
