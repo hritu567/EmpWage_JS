5ce9a0294708de2d5135d73042f3698d4902b5e7
hritu567 <hritumog96@gmail.com>
2020-11-20 19:16:13 +0530

    Added : Employee wages for multiple companies

--- EmpWage.js
+++ EmpWage.js
@@ -1,3 +1,6 @@
 console.log("Welcome to Employee Wage Computation Program");
 let Utility=require('./utility');
-Utility.Employee();
+Utility.EmployeeWages('Toyota', 20, 15, 160);
+Utility.EmployeeWages('Apple', 50, 4, 140);
+
+
--- utility.js
+++ utility.js
@@ -1,34 +1,29 @@
-//uc7
-//Refactoring UC6 using function
+//uc8
+//Calculating Wages for multiple companies
 const IS_FULL_TIME = 1;
 const IS_PART_TIME = 2;
-const EMP_RATE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 20;
-const MAX_HRS_IN_MONTH = 100;
-function Employee(){
-var empHrs = 0;
-var totalSalary = 0;
-var totalEmpHrs = 0;
-var totalWorkingDays = 0;
-while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
-    totalWorkingDays++;
-    var empCheck = Math.floor(Math.random() * 10) % 3;
+function EmployeeWages(comname, empRatePerHour, numOfWorkingDays, maxHoursInMonth){
+let empHrs = 0;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while (totalEmpHrs < maxHoursInMonth && totalWorkingDays < numOfWorkingDays){
+	totalWorkingDays++;
+	var empCheck = Math.floor(Math.random() * 10) % 3;
 	switch (empCheck) {
-		case 1:
+		case IS_FULL_TIME:
 			empHrs = 8;
 			break;
-		case 2:
+		case IS_PART_TIME:
 			empHrs = 4;
 			break;
 		default:
 			empHrs = 0;
 			break;
             }
-    totalEmpHrs += empHrs;
-	console.log("Day : " + totalWorkingDays + " Emp hours : " + empHrs);
-    
+    totalEmpHrs = totalEmpHrs + empHrs;
+	console.log("Day : " , totalWorkingDays + " Emp hours : " , empHrs);
+    }
+let totalSalary = empRatePerHour * totalEmpHrs;
+console.log("Total Salary for : " , comname + " is " , totalSalary);
 }
-totalSalary = EMP_RATE_PER_HOUR * totalEmpHrs;
-console.log("Total Salary for a month is : " + totalSalary);
-}
-module.exports = { Employee }
+module.exports = { EmployeeWages }
